e573340d1bf8a91af5b6f8dacccc07c2197b2898
fyusuf-a <fyusuf-a@student.42.fr>
2020-05-29 13:31:04 +0200

    libft: forgot in last commit ft_lstadd_elem.c

---
+++ libft/ft_lstadd_elem.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyusuf-a <fyusuf-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/29 12:46:24 by fyusuf-a          #+#    #+#             */
+/*   Updated: 2020/05/29 12:46:39 by fyusuf-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int				ft_lstadd_elem(t_list **alst, void *data)
+{
+	t_list	*new;
+
+	if (!alst)
+		return (1);
+	if (!(new = malloc(sizeof(t_list))))
+		return (1);
+	new->content = data;
+	new->next = *alst;
+	*alst = new;
+	return (0);
+}
